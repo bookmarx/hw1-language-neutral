284fa5d3321fbbcce92ec2fcf7ebfa369628f28d
Edward Lau <eklau@ucsd.edu>
2016-04-07 20:25:09 -0700

    java in progress

--- html/java/hello.java
+++ html/java/hello.java
@@ -1,9 +1,27 @@
 import java.util.*; 
 import java.io.*;
+
 class hello
 {
   public static void main( String args[] ) {
 	System.out.println("Content-Type: text/html\n\n");
-	System.out.println("Hi");
+
+  Random randomGenerator = new Random();
+
+  int randInt = randomGenerator.nextInt(15);
+  
+  privat String[] colors = {"aqua", "black", "blue", "fuchsia",
+                            "gray", "green", "lime", "maroon",
+                            "navy", "olive", "purple", "red",
+                            "silver", "teal", "white", "yellow"};
+
+  System.out.println("<html> 
+  <head><title>Java CGI Script</title></head>
+  <body bgcolor=color[randInt]>");
+
+  Date d = new Date();
+  DateFormat df = DateFormat.getDateInstance();
+  System.out.println(df.format(d));
+  
   }
 }
